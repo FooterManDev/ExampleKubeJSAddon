4f84f0b7f8d725b6b36245917719c1529b5aedb7
FooterManDev <footerman.work@gmail.com>
2024-08-17 10:14:04 -0400

    move around comments

--- src/main/java/com/example/exampleaddon/kubejs/ExampleKubeJSPlugin.java
+++ src/main/java/com/example/exampleaddon/kubejs/ExampleKubeJSPlugin.java
@@ -3,18 +3,17 @@ package com.example.exampleaddon.kubejs;
 import com.example.exampleaddon.ExampleAddon;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 
+/* Basic example of a KubeJS Plugin.
+       To register your own plugins, add this class and package name to "kubejs.plugins.txt" in your Resources directory.
+*/
 public class ExampleKubeJSPlugin implements KubeJSPlugin
 {
-    /* Basic example of a KubeJS Plugin.
-       To register your own plugins, add this class and package name to "kubejs.plugins.txt" in your Resources directory.
-    */
-
+    /** If you don't know how to add content, use Kube's built-in Plugin for reference.
+     @see dev.latvian.mods.kubejs.BuiltinKubeJSPlugin
+     */
     @Override
     public void init() {
-        ExampleAddon.LOGGER.info("This is my KubeJS Plugin!");
-        /** If you don't know how to add content, use Kube's built-in Plugin for reference.
-        @see dev.latvian.mods.kubejs.BuiltinKubeJSPlugin
-         */
+        ExampleAddon.LOGGER.info("KubeJS Plugin Loaded.");
     }
 }
 
